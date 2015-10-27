c8410953e3aa4d5b27032f1f9c256485d631a07e
zouxiangwen <jxxiangwen@live.com>
2015-10-27 10:22:13 +0800

    遗传算法求文档权重

--- cn/edu/shu/match/test.py
+++ cn/edu/shu/match/test.py
@@ -10,7 +10,13 @@ import json
 # write_algorithm = open('./config/table.json', encoding='utf-8', mode='w')
 # json_data["provide"] = "provide我的"
 # json.dump(json_data, write_algorithm, ensure_ascii=False, indent=4, sort_keys=True)
-with open('./result/score.txt', encoding='utf-8', mode='a+') as score_file:
-    score_file.write(str(2.3)+'\n')
-    score_file.writelines(["1","2"] )
-    score_file.write(str(2.3))
+
+
+# with open('./result/score.txt', encoding='utf-8', mode='a+') as score_file:
+#     score_file.write(str(2.3)+'\n')
+#     score_file.writelines(["1","2"] )
+#     score_file.write(str(2.3))
+with open('./config/genetic.json', encoding='utf-8') as genetic_file:
+    genetic_json = json.load(genetic_file)
+    with open('./config/algorithm.json', encoding='utf-8') as algorithm_file:
+        min = int(['min'])
